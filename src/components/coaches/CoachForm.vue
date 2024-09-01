d00980946ed9f037f4f0eab97fc420256b774c03
akmalzidani <akmal20031003@gmail.com>
2024-09-01 21:56:33 +0700

    add form validation

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -2,22 +2,76 @@
 import { ref } from 'vue'
 
 const formValues = ref({
-  firstName: '',
-  lastName: '',
-  description: '',
-  rate: null,
-  areas: []
+  firstName: {
+    val: '',
+    isValid: true
+  },
+  lastName: {
+    val: '',
+    isValid: true
+  },
+  description: {
+    val: '',
+    isValid: true
+  },
+  rate: {
+    val: null,
+    isValid: true
+  },
+  areas: {
+    val: [],
+    isValid: true
+  },
+  formIsValid: true
 })
 
 const emit = defineEmits(['save-data'])
 
+const clearValidity = (field) => {
+  formValues.value[field].isValid = true
+}
+
+const validateForm = () => {
+  formValues.value.formIsValid = true
+  if (formValues.value.firstName.val.trim() === '') {
+    formValues.value.firstName.isValid = false
+    formValues.value.formIsValid = false
+  }
+  if (formValues.value.lastName.val.trim() === '') {
+    formValues.value.lastName.isValid = false
+    formValues.value.formIsValid = false
+  }
+  if (formValues.value.description.val.trim() === '') {
+    formValues.value.description.isValid = false
+    formValues.value.formIsValid = false
+  }
+  if (
+    formValues.value.rate.val === null ||
+    formValues.value.rate.val < 0 ||
+    !formValues.value.rate.val
+  ) {
+    formValues.value.rate.isValid = false
+    formValues.value.formIsValid = false
+  }
+  if (formValues.value.areas.val.length === 0) {
+    formValues.value.areas.isValid = false
+    formValues.value.formIsValid = false
+  }
+}
+
 const submitForm = () => {
+  validateForm()
+
+  if (!formValues.value.formIsValid) {
+    return
+  }
+
   const formData = {
-    first: formValues.value.firstName,
-    last: formValues.value.lastName,
-    desc: formValues.value.description,
-    rate: formValues.value.rate,
-    areas: formValues.value.areas
+    first: formValues.value.firstName.val,
+    last: formValues.value.lastName.val,
+    desc: formValues.value.description.val,
+    rate: formValues.value.rate.val,
+    areas: formValues.value.areas.val
   }
 
   emit('save-data', formData)
@@ -26,37 +80,82 @@ const submitForm = () => {
 
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !formValues.firstName.isValid }">
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" v-model.trim="formValues.firstName" />
+      <input
+        type="text"
+        id="firstname"
+        v-model.trim="formValues.firstName.val"
+        @input="clearValidity('firstName')"
+      />
+      <p v-if="!formValues.firstName.isValid">Firstname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !formValues.lastName.isValid }">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" v-model.trim="formValues.lastName" />
+      <input
+        type="text"
+        id="lastname"
+        v-model.trim="formValues.lastName.val"
+        @input="clearValidity('lastName')"
+      />
+      <p v-if="!formValues.lastName.isValid">Lastname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !formValues.description.isValid }">
       <label for="description">Description</label>
-      <textarea type="text" id="description" rows="5" v-model.trim="formValues.description" />
+      <textarea
+        type="text"
+        id="description"
+        rows="5"
+        v-model.trim="formValues.description.val"
+        @input="clearValidity('description')"
+      />
+      <p v-if="!formValues.description.isValid">Description must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !formValues.rate.isValid }">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="formValues.rate" />
+      <input
+        type="number"
+        id="rate"
+        v-model.number="formValues.rate.val"
+        @input="clearValidity('rate')"
+      />
+      <p v-if="!formValues.rate.isValid">Rate must be greater than 0.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !formValues.areas.isValid }">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" v-model="formValues.areas" id="frontend" value="frontend" />
+        <input
+          type="checkbox"
+          v-model="formValues.areas.val"
+          @change="clearValidity('areas')"
+          id="frontend"
+          value="frontend"
+        />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" v-model="formValues.areas" id="backend" value="backend" />
+        <input
+          type="checkbox"
+          v-model="formValues.areas.val"
+          @change="clearValidity('areas')"
+          id="backend"
+          value="backend"
+        />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" v-model="formValues.areas" id="career" value="career" />
+        <input
+          type="checkbox"
+          v-model="formValues.areas.val"
+          @change="clearValidity('areas')"
+          id="career"
+          value="career"
+        />
         <label for="career">Career Advisory</label>
       </div>
+      <p v-if="!formValues.areas.isValid">At least one expertise must be selected.</p>
     </div>
+    <p v-if="!formValues.formIsValid">Please fix the above errors and submit again.</p>
     <BaseButton>Register</BaseButton>
   </form>
 </template>
