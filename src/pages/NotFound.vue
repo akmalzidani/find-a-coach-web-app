bf229cc54b02212cd831e5063d4c19045a8f46cb
akmalzidani <akmal20031003@gmail.com>
2024-09-06 14:17:21 +0700

    add notfound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <div>NOT FOUND</div>
+  <section>
+    <BaseCard>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <RouterLink to="/coaches">coaches</RouterLink>
+      </p>
+    </BaseCard>
+  </section>
 </template>
